f07effb464b08737e19df07e720267ed46b30960
wangzhao <13132398914@163.com>
2020-12-30 22:08:47 +0800

    使用人人generator生成优惠服务后台代码

--- coupon/src/main/java/org/simple/mall/coupon/CouponApplication.java
+++ coupon/src/main/java/org/simple/mall/coupon/CouponApplication.java
@@ -1,9 +1,11 @@
 package org.simple.mall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.simple.mall.coupon.dao")
 public class CouponApplication {
 
     public static void main(String[] args) {
